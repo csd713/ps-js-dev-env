bceab6adfc0d6b9857e3d6bdf252c506b78475f1
Chiranjeevi SD <jeevi146@gmail.com>
2018-02-07 16:30:57 -0500

    test build fail using Travis CI

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('My first test', ()=>{
 	it('should pass', ()=>{
 		expect(true).to.equal(true);
+	});
+});
+
+describe('index.html', ()=>{
+	it('should say hello', (done)=>{
+		//reference for index.html file
+		const index = fs.readFileSync('./src/index.html', "utf-8");
+		jsdom.env(index, function(err, window){
+			const h1 = window.document.getElementsByTagName('h1')[0];
+			expect(h1.innerHTML).to.equal("Hello world!");
+			done();
+			window.close();
+		});
 	});
 });
